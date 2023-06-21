405cd09553fea593a5976e96893f12786eb12826
rahul-fastapp <rahul@fastapp.in>
2023-06-21 12:59:07 +0530

    fix url issue

--- frontend/src/SVG_Viewer.js
+++ frontend/src/SVG_Viewer.js
@@ -23,7 +23,7 @@ function SVG_Viewer(props) {
         formData.append("file", props.imageData);
         try {
           // const response = await fetch("http://127.0.0.1:5000/pdf_to_image", {
-          const response = await fetch("https://demo.getafto.com/pdf_to_image", {
+          const response = await fetch("/pdf_to_image/", {
             method: "POST",
             body: formData,
           });
